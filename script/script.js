0bf321fce2dba14dd39d235fee8a07dd4f7ddd49
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2024-07-19 22:25:32 +0330

    add

--- script/script.js
+++ script/script.js
@@ -7,11 +7,10 @@ const wrapper = document.querySelector(".wrapper"),
 let imageIndex = 1,
   intervalId;
 
-
 // Define function to start automatic image slider
 const autoSlide = () => {
-    // Start the slideshow by calling slideImage() every 2 seconds
-    intervalId = setInterval(() => slideImage(++imageIndex), 2000);
-  };
-  // Call autoSlide function on page load
-  autoSlide();
+  // Start the slideshow by calling slideImage() every 2 seconds
+  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
+};
+// Call autoSlide function on page load
+autoSlide();
