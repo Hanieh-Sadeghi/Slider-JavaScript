fd51afd22d34e0a868cecaae2c24b369d741ec50
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2024-07-20 05:58:59 +0330

    Key Changes

--- script/script.js
+++ script/script.js
@@ -7,43 +7,37 @@ const wrapper = document.querySelector(".wrapper"),
 let imageIndex = 1,
   intervalId;
 
-// Define function to start automatic image slider
-
-const autoSlide = () => {
-  // Start the slideshow by calling slideImage() every 2 seconds
-
-  intervalId = setInterval(() => slideImage(++imageIndex), 2000);
-};
-
-// Call autoSlide function on page load
-autoSlide();
-
 // A function that updates the carousel display to show the specified image
-
 const slideImage = () => {
   // Calculate the updated image index
-
   imageIndex =
-    imageIndex === images.length
+    imageIndex >= images.length
       ? 0
       : imageIndex < 0
       ? images.length - 1
       : imageIndex;
 
   // Update the carousel display to show the specified image
+  carousel.style.transform = `translateX(-${imageIndex * 100}%)`;
+};
 
-  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+// Define function to start automatic image slider
+const autoSlide = () => {
+  // Start the slideshow by calling slideImage() every 2 seconds
+  intervalId = setInterval(() => {
+    imageIndex++;
+    slideImage();
+  }, 2000);
 };
 
 // A function that updates the carousel display to show the next or previous image
-
 const updateClick = (e) => {
   // Stop the automatic slideshow
   clearInterval(intervalId);
 
   // Calculate the updated image index based on the button clicked
   imageIndex += e.target.id === "next" ? 1 : -1;
-  slideImage(imageIndex);
+  slideImage();
 
   // Restart the automatic slideshow
   autoSlide();
@@ -57,3 +51,6 @@ wrapper.addEventListener("mouseover", () => clearInterval(intervalId));
 
 // Add mouseleave event listener to wrapper element to start auto sliding again
 wrapper.addEventListener("mouseleave", autoSlide);
+
+// Call autoSlide function on page load
+autoSlide();
