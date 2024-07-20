f8b24638e555f6a05175a260d93f9e328a9de2e5
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2024-07-20 05:10:20 +0330

    add fuc slideImage

--- script/script.js
+++ script/script.js
@@ -8,9 +8,31 @@ let imageIndex = 1,
   intervalId;
 
 // Define function to start automatic image slider
+
 const autoSlide = () => {
+
   // Start the slideshow by calling slideImage() every 2 seconds
+
   intervalId = setInterval(() => slideImage(++imageIndex), 2000);
 };
 // Call autoSlide function on page load
+
 autoSlide();
+
+// A function that updates the carousel display to show the specified image
+
+const slideImage = () => {
+
+  // Calculate the updated image index
+
+  imageIndex =
+    imageIndex === images.length
+      ? 0
+      : imageIndex < 0
+      ? images.length - 1
+      : imageIndex;
+
+  // Update the carousel display to show the specified image
+
+  carousel.style.transform = `translate(-${imageIndex * 100}%)`;
+};
